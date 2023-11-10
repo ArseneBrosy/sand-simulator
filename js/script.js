4bd8b792cdca7facba4130f52a44f6ac01ffb6f9
ArseneBrosy <arsene.brosy@divtec.ch>
2023-11-10 13:47:08 +0100

    found optimisation : step()

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@
 // by Arsène Brosy
 let canvas = document.getElementById("game");
 let ctx = canvas.getContext("2d");
-canvas.width = 100;
-canvas.height = 100;
+let fpsCounter = document.getElementById("fps");
+canvas.width = 300;
+canvas.height = 300;
 
 //region CONSTANTES
 const SAND_FRICTION = .9;
@@ -18,33 +19,39 @@ let canvasMouseY = 0;
 let mouseButtonPressed = -1;
 let penRadius = 10;
 
+let materials = [
+  {
+    material: 0,
+    tags: [],
+    red: 255,
+    green: 255,
+    blue: 255
+  },
+  {
+    material: 1,
+    tags: [],
+    red: 0,
+    green: 0,
+    blue: 0
+  },
+  {
+    material: 2,
+    tags: [],
+    red: 255,
+    green: 255,
+    blue: 0
+  }
+];
+
 // world
 let world = [];
 for (let y = 0; y < canvas.height; y++) {
   let newLine = [];
   for (let x = 0; x < canvas.width; x++) {
-    newLine.push({
-      material: 0,
-      tags: []
-    });
+    newLine.push(materials[0]);
   }
   world.push(newLine);
 }
-
-let materials = [
-  {
-    material: 0,
-    tags: []
-  },
-  {
-    material: 1,
-    tags: ["collider"]
-  },
-  {
-    material: 2,
-    tags: ["collider"]
-  }
-];
 //endregion
 
 //region FUNCTIONS
@@ -80,53 +87,51 @@ setInterval(() => {
   }
   //endregion
 
+  let dt = new Date();
+  let startedFrameTime = dt.getSeconds() * 1000 + dt.getMilliseconds();
   draw()
-}, 0);
-
-setInterval(() => {
-  step();
-  //draw();
+  step()
+  dt = new Date();
+  let frameTime = dt.getSeconds() * 1000 + dt.getMilliseconds() - startedFrameTime;
+  fpsCounter.innerHTML = Math.floor(1000 / frameTime).toString();
 }, 0);
 
 function step() {
-  let newWorld = JSON.parse(JSON.stringify(world));
   for (let y = 0; y < canvas.height; y++) {
     for (let x = 0; x < canvas.width; x++) {
       // sand
       if (world[y][x].material === 2 && y < canvas.height - 1) {
         if (world[y + 1][x].material === 0) {
-          newWorld[y + 1][x] = materials[2];
-          newWorld[y][x] = materials[0];
+          world[y + 1][x] = materials[2];
+          world[y][x] = materials[0];
         }
         else if (x < canvas.width - 1 && world[y + 1][x + 1].material === 0 && Math.random() > SAND_FRICTION) {
-          newWorld[y + 1][x + 1] = materials[2];
-          newWorld[y][x] = materials[0];
+          world[y + 1][x + 1] = materials[2];
+          world[y][x] = materials[0];
         }
         else if (x > 0 && world[y + 1][x - 1].material === 0 && Math.random() > SAND_FRICTION) {
-          newWorld[y + 1][x - 1] = materials[2];
-          newWorld[y][x] = materials[0];
+          world[y + 1][x - 1] = materials[2];
+          world[y][x] = materials[0];
         }
       }
     }
   }
-  world = JSON.parse(JSON.stringify(newWorld));
 }
 
 function draw() {
-  // clear
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
   // world
-  for (let y = 0; y < canvas.height; y++) {
-    for (let x = 0; x < canvas.width; x++) {
-      switch (world[y][x].material) {
-        case 0: ctx.fillStyle = "white"; break;
-        case 1: ctx.fillStyle = "black"; break;
-        case 2: ctx.fillStyle = "yellow"; break;
-      }
-      ctx.fillRect(x, y, 1, 1);
-    }
+  for (let i = 0; i < imageData.data.length; i+=4) {
+    let x = (i / 4) % canvas.width;
+    let y = Math.floor((i / 4) / canvas.width);
+    imageData.data[i] = world[y][x].red;
+    imageData.data[i + 1] = world[y][x].green;
+    imageData.data[i + 2] = world[y][x].blue;
+    imageData.data[i + 3] = 255;
   }
+
+  ctx.putImageData(imageData, 0, 0);
 }
 
 canvas.addEventListener("mousemove", (e) => {
